f0565295bb6d039a2c4368942818b764ce494c2a
bigtime007 <harrigan.kev@gmail.com>
2023-03-19 15:57:27 -0500

    progress

---
+++ section-2-js-fundamentals/script.js
@@ -0,0 +1,55 @@
+// let js = 'mainframe'
+// if (js === 'mainframe') alert("mainframe activation online");
+// console.log(40 + 8 + 23 -10);
+
+
+console.log("Jonas");
+console.log(23);
+
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3; // Syntax error bad naming
+
+let jonas_matilda = "JM";
+
+// let function = 23; // illegal name
+
+//  let Person = 'hello'; // convention issue
+
+let PI = 3.1415 // sets as a constant when upper case.
+
+let myFirstJob = "programmer";
+let myCurrentJob = "Teacher"
+
+let job1 = 'programmer'; // bad var naming
+let job2 = "teacher"; // bad var naming
+
+console.log(myFirstJob)
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log("Type: " + typeof javaScriptIsFun);
+console.log("Type: " + typeof 23)
+
+// reasign value
+javaScriptIsFun = "YES!"
+
+console.log("Type: " + typeof javaScriptIsFun);
+
+// legal to define var without value.
+let year;
+console.log(year);
+console.log("Type: " + typeof year);
+
+year = 1991;
+console.log("Type: " + typeof year);
+console.log(year);
+console.log("Type BUG: " + typeof null); // bug in js is viewed as a object??? 
+
+// 12. Data Types
+
